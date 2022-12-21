2513c83a041e248697a71688240272a326a9e65d
Subhadiptech <subhadipdhn@gmail.com>
2022-12-21 16:55:21 +0530

    Migration from gradle to DSL

--- app/src/main/java/com/ersubhadip/caloriemanager/ui/theme/Theme.kt
+++ app/src/main/java/com/ersubhadip/caloriemanager/ui/theme/Theme.kt
@@ -32,7 +32,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun CaloryTrackerTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun CalorieManagerTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
